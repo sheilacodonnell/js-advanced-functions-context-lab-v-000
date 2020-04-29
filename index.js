58822b5ed174ae9180a4ee26dfc34290b8692dd2
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-29 20:18:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,42 +36,42 @@ function createEmployeeRecords(records) {
   return records.map(record => createEmployeeRecord(record) )
 }
 
-function createTimeInEvent(record, dateStamp) {
-  let dateAndHour = dateStamp.split(" ")
-  record.timeInEvents.push({
-    type: "TimeIn",
-    date: dateAndHour[0],
-    hour: parseInt(dateAndHour[1])
-  })
-  return record
-}
+// function createTimeInEvent(record, dateStamp) {
+//   let dateAndHour = dateStamp.split(" ")
+//   record.timeInEvents.push({
+//     type: "TimeIn",
+//     date: dateAndHour[0],
+//     hour: parseInt(dateAndHour[1])
+//   })
+//   return record
+// }
 
-function createTimeOutEvent(record, dateStamp) {
-  let dateAndHour = dateStamp.split(" ")
-  record.timeOutEvents.push({
-    type: "TimeOut",
-    date: dateAndHour[0],
-    hour: parseInt(dateAndHour[1])
-  })
-  return record
-}
+// function createTimeOutEvent(record, dateStamp) {
+//   let dateAndHour = dateStamp.split(" ")
+//   record.timeOutEvents.push({
+//     type: "TimeOut",
+//     date: dateAndHour[0],
+//     hour: parseInt(dateAndHour[1])
+//   })
+//   return record
+// }
 
-function hoursWorkedOnDate(record, date) {
-  let outHour = record.timeOutEvents.find(element => element.date === date).hour
-  let inHour = record.timeInEvents.find(element => element.date === date).hour
-  return (outHour - inHour) /100
-}
+// function hoursWorkedOnDate(record, date) {
+//   let outHour = record.timeOutEvents.find(element => element.date === date).hour
+//   let inHour = record.timeInEvents.find(element => element.date === date).hour
+//   return (outHour - inHour) /100
+// }
 
-function wagesEarnedOnDate(record, date) {
-  let hours = hoursWorkedOnDate(record, date)
-  return hours * record.payPerHour
-}
+// function wagesEarnedOnDate(record, date) {
+//   let hours = hoursWorkedOnDate(record, date)
+//   return hours * record.payPerHour
+// }
 
 
-function calculatePayroll(records) {
-  return records.reduce((total, employee) => total + allWagesFor(employee), 0)
-}
+// function calculatePayroll(records) {
+//   return records.reduce((total, employee) => total + allWagesFor(employee), 0)
+// }
 
-function findEmployeeByFirstName(srcArray, firstName) {
-    return srcArray.find(employee => employee.firstName == firstName)
-}
+// function findEmployeeByFirstName(srcArray, firstName) {
+//     return srcArray.find(employee => employee.firstName == firstName)
+// }
